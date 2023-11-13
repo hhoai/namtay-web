982c76aae289534d08e5d75d095588642246fc9d
hhoai <hhoaii261@gmail.com>
2023-11-13 21:29:35 +0700

    validation login

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,6 +1,6 @@
 function Contact() {
   return ( 
-    <div>affiliate</div>
+    <div></div>
    );
 }
 
--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import "./LogIn.css";
 
 const btn = [
@@ -13,18 +14,69 @@ const btn = [
   },
 ];
 
-const form = [
-  {
-    placeholder: "Email",
-    type: "email",
-  },
-  {
-    placeholder: "Mật khẩu",
-    type: "password",
-  },
-];
-
 function LogIn() {
+  // const inputEmail = useRef();
+
+  // const validationForm = () => {};
+  // const handleBtn = () => {
+  //   console.log(logInBtn.current);
+  // };
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  // Create refs for input fields
+
+  // State for error messages
+  const [errorMessage, setErrorMessage] = useState("Email và mật khẩu không được để trống!");
+
+  function validateEmail(email) {
+    // Regular expression pattern for a valid email address
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+    // Use the test method to check if the email matches the pattern
+    return emailPattern.test(email);
+  }
+
+  // Validation function
+  const validateForm = () => {
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (!username || !password) {
+      setErrorMessage(`${errorMessage}`);
+      alert(errorMessage);
+      return false;
+    }
+
+    if (!validateEmail(username)) {
+      alert("Email không đúng định dạng!");
+      return false;
+    }
+
+    // You can add more complex validation here, e.g., checking for the format of the username and password.
+
+    return true;
+  };
+
+  // Form submission handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Clear previous error message
+    setErrorMessage("");
+
+    // Validate the form
+    if (validateForm()) {
+      const form = [{username:usernameRef.current.value, password:passwordRef.current.value}];
+      usernameRef.current.value = "";
+      passwordRef.current.value = "";
+      // Perform your login logic here
+      // You can send the data to the server or handle authentication as needed
+      console.log(form);
+      alert("Đăng nhập thành công!");
+    }
+  };
+
   return (
     <div className="w-[1160px] m-auto">
       <div className="header flex items-center justify-between h-[100px]">
@@ -60,19 +112,28 @@ function LogIn() {
             </p>
           </div>
           <div className="flex flex-col">
-            {form.map((item) => (
-              <input
-                type={item.type}
-                placeholder={item.placeholder}
+          <input
+                type="email" placeholder="email"
+                ref={usernameRef}
                 className="border-[1px] border-black border-solid rounded-xl p-3 mb-5 text-lg hover:shadow-[0_0_6px_0_rgba(96,79,201,.5019607843137255)]"
               />
-            ))}
+            <input
+                type="password"
+                placeholder="password"
+                ref={passwordRef}
+                className="border-[1px] border-black border-solid rounded-xl p-3 mb-5 text-lg hover:shadow-[0_0_6px_0_rgba(96,79,201,.5019607843137255)]"
+              />
           </div>
-          <button className="w-[100%] rounded-xl text-[var(--white)] text-xl font-bold bg-[var(--primary)] py-6 mb-5">
+          <button
+            onClick={handleSubmit}
+            className="w-[100%] rounded-xl text-[var(--white)] text-xl font-bold bg-[var(--primary)] py-6 mb-5"
+          >
             Đăng nhập
           </button>
           <a href="/quen-mat-khau">
-            <span className="text-lg font-medium mb-5 hover">Quên mật khẩu?</span>
+            <span className="text-lg font-medium mb-5 hover">
+              Quên mật khẩu?
+            </span>
           </a>
         </div>
         <div>
@@ -88,5 +149,3 @@ function LogIn() {
 }
 
 export default LogIn;
-
-
